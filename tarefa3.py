262758db15245048748d9c50008dec68ef5b7576
Dalmir Nogueira <dalmir.nogueira@outlook.com>
2022-03-22 06:43:27 -0300

    Add files via upload

---
+++ tarefa3.py
@@ -0,0 +1,23 @@
+listaCaractere = ["mesa", "cadeira", "cadeira"]
+listaInteiro = [1, 5, 7, 9, 3, 5]
+listaBooleano = [True, False, False]
+
+print (listaCaractere)
+print (listaInteiro)
+print (listaBooleano)
+
+listaCaractere.append("copo")
+listaInteiro.insert(8, 1)
+listaInteiro.remove(5)
+print(listaBooleano.pop(0))
+print (listaCaractere)
+del listaCaractere[2]
+print (listaCaractere)
+print (listaInteiro)
+print (listaBooleano)
+
+listaInteiro.append(0)
+print("Lista depois do append: ", listaInteiro)
+print("Elemento removido: ", listaInteiro.pop())
+print("Lista depois do pop: ", listaInteiro)
+
